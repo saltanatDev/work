64a8b64249b5b09d8007db26a0576189710e9845
saltanatDev <saltanatkamalova558@gmail.com>
2022-10-16 13:48:11 +0600

    init

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let swiper = new swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
